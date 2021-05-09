7d8c1e2a62bda0b10fb63863f09cb6a7f0eb9163
charlloss.dev <charlloss.dev@gmail.com>
2021-05-10 04:13:03 +0900

    feat(stack): update t10799 algorithm

--- src/test/java/stack/t10799.java
+++ src/test/java/stack/t10799.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -11,33 +13,61 @@ import common.Common;
 public class t10799 {
 
 	@ParameterizedTest
-	@ValueSource(strings = {"()(((()())(())()))(())"})
+	@ValueSource(strings = {"()(((()())(())()))(())", "(((()(()()))(())()))(()())"})
 	void test(String input) {
 		Scanner sc = new Scanner(Common.systemIn(input));
 		String msg = sc.nextLine();
-		Stack<work> stack = new Stack<>();
+		List<pipe> pipes = new ArrayList<>();
+		Stack<Integer> temp = new Stack<>();
+		Stack<Integer> cut = new Stack<>();
 		int position = 0;
 		for (String s : msg.split("")) {
 			if (s.equals(")")) {
-				
+				if (temp.peek() == position - 1) {
+					cut.push(position);
+					temp.pop();
+				} else {
+					pipes.add(new pipe(temp.pop(), position));
+				}
 			} else {
-				stack.push(new work(position, s));
+				temp.push(position);
 			}
 			position++;
 		}
-	}
 
-	private class work {
-		private final int position;
-		private final String data;
-
-		public work(int pos, String data) {
-			this.position = pos;
-			this.data = data;
+		while (!cut.isEmpty()) {
+			int point = cut.pop();
+			List<pipe> result = new ArrayList<>();
+			for (pipe p : pipes) {
+				if (p.isCut(point)) {
+					result.add(new pipe(p.start, point - 1));
+					result.add(new pipe(point + 1, p.end));
+				} else {
+					result.add(p);
+				}
+			}
+			pipes = result;
 		}
 
-		public boolean isLager(int pos) {
-			return pos - this.position == -1;
+		System.out.println(pipes.size());
+	}
+
+	private static class pipe {
+		private final int start;
+		private final int end;
+
+		@Override
+		public String toString() {
+			return "[" + start + ", " + end + "]";
+		}
+
+		private pipe(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		public boolean isCut(int point) {
+			return this.start < point && end > point;
 		}
 	}
 }
